3517df04e22d649ac187dbfab973f8f19f0d4b79
eztaah <eztaah@gmail.com>
2024-01-24 14:28:54 +0100

    Refactor player deplacement script - Setup input map - Use move_and_slide() method for handling future collisions - Player class creation

---
+++ scripts/player.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+var DEADZONE = 0.2
+
+
+func _physics_process(delta):
+	# On stocke la direction du joueur dans un Vector2
+	var direction: Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	
+	# On normalise la vitesse
+	velocity = direction.normalized() * SPEED
+	
+	# On actualise la position + gestions des collisions
+	move_and_slide()
+	
+	
+	####################################################################33
+	##On récupère les axes de déplacement de la manette
+	#var movementVector = Vector2(Input.get_joy_axis(0, JOY_AXIS_LEFT_X), Input.get_joy_axis(0,JOY_AXIS_LEFT_Y))
+	#
+	##Si la magnitude du mouvement est plus grande que la deadzone -> on bouge, sinon à on reste à l'arrêt
+	#if movementVector.length() > DEADZONE:
+		#
+		##Normalisation du vecteur (évite les mouvement plus rapides si on va en diagonale -> car sans ca on additionnerait la vitesse up et right par ex)
+		#movementVector = movementVector.normalized()
+		#
+		##Déplacement du sprite
+		#position += movementVector * speed * delta
+		#print(movementVector)
+	#else:
+		#position += Vector2.ZERO

75c3ba067f14411ead7b693d42166c85864832a7
eztaah <eztaah@gmail.com>
2024-01-30 10:21:27 +0100

    Creation scene pour collectable_stone 2

--- src/scripts/collectable_stone.gd
+++ src/scripts/collectable_stone.gd
@@ -4,13 +4,12 @@ var player_instance
 var isInRange = false
 
 func _physics_process(_delta):
-	#if isInRange == true and player_instance.input.is_action_just_pressed("ramasser_pierre") and player_instance.rock_stocked == 0:
-		##player_instance.rock_equiped = true
-		#player_instance.rock_stocked = 1
-		#player_instance.SPEED = 150.0
-		#isInRange = false
-		#queue_free()
-	pass
+	if isInRange == true and player_instance.input.is_action_just_pressed("ramasser_pierre") and player_instance.rock_stocked == 0:
+		#player_instance.rock_equiped = true
+		player_instance.rock_stocked = 1
+		player_instance.SPEED = 150.0
+		isInRange = false
+		queue_free()
 
 func _on_body_entered(body):
 	if is_instance_valid(body) and body is CharacterBody2D:
